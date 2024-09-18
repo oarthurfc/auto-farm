d903bc8b5792808364a740a31e84bc1b51699334
oarthurfc <arthuryouface@gmail.com>
2024-09-18 12:10:24 -0300

    Adicionando estilos globais

--- Codigo/front-end/auto-farm/tailwind.config.js
+++ Codigo/front-end/auto-farm/tailwind.config.js
@@ -5,8 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Poppins',
+      },
+      boxShadow: {
+        shape: '0px 10px 66px rgba(0, 0, 0, 0.1)'
+      }
+    }
   },
   plugins: [],
 }
-
